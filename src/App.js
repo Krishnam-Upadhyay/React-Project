9b89d4eadb38faf26aa58b2b52b9ac439e23f76f
CodeSandbox <support@codesandbox.io>
2023-09-23 00:35:04 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./styles.css";
-import UseStateArray from "./hooks/UseEffect/FetchUser";
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Products from "./Components/Products";
+
+
+
+const products = [
+  {
+    pid: 1,
+    name: 'fresh Milk',
+    desc: 'Here we will show the details of teh product in order to solve the problem',
+    isAvailable: true
+  }
+]
 
 export default function App() {
-  return (
-    <>
 
-      <UseStateArray />
-    </>
 
-  );
+
+  return <div className="row">
+    <div className="col-lg-8 mx-auto">
+      <Products />
+
+    </div>
+
+
+  </div>
 }
