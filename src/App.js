a23f796b5c6549b24a7f921b6c32946eaddab992
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2023-09-26 01:45:07 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ import Products from "./Components/Products";
 
 
 
-const products = [
-  {
-    pid: 1,
-    name: 'fresh Milk',
-    desc: 'Here we will show the details of teh product in order to solve the problem',
-    isAvailable: true
-  }
-]
+
 
 export default function App() {
 
