6bcca2acadc69c52889ebae7f78145daf4d34103
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2023-09-26 02:00:28 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Products from "./Components/Products";
 
 
-<<<<<<< HEAD
 
 
 
-=======
+
+
+
 let products = [
   {
     pID: 1,
@@ -50,7 +51,7 @@ let products = [
     price: 14
   }
 ]
->>>>>>> f19bbd7c47bb73928742947d52dec76a91161a23
+
 
 
 
@@ -60,7 +61,47 @@ export default function App() {
 
   return <div className="row">
     <div className="col-lg-8 mx-auto">
-      <Products />
+      <Products
+        id={products[0].pID}
+        name={products[0].pName}
+        description={products[0].desc}
+        isAvailable={products[0].isAvailable}
+        imageUrl={products[0].image}
+        price={products[0].price}
+      />
+      <Products
+        id={products[1].pID}
+        name={products[1].pName}
+        description={products[1].desc}
+        isAvailable={products[1].isAvailable}
+        imageUrl={products[1].image}
+        price={products[1].price}
+      />
+      <Products
+        id={products[2].pID}
+        name={products[2].pName}
+        description={products[2].desc}
+        isAvailable={products[2].isAvailable}
+        imageUrl={products[2].image}
+        price={products[2].price}
+      />
+      <Products
+        id={products[3].pID}
+        name={products[3].pName}
+        description={products[3].desc}
+        isAvailable={products[3].isAvailable}
+        imageUrl={products[3].image}
+        price={products[3].price}
+      />
+      <Products
+        id={products[4].pID}
+        name={products[4].pName}
+        description={products[4].desc}
+        isAvailable={products[4].isAvailable}
+        imageUrl={products[4].image}
+        price={products[4].price}
+      />
+
 
     </div>
 
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -10,22 +10,22 @@ const style = {
 let isAvailable = 'Available';
 let badgeClass = 'badge-margin-left-240 badge ';
 
-const Products = () => {
+const Products = (props) => {
 
     return (<ul class="list-group shadow">
         <li className="list-group-item">
             <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                 <div className="media-body order-2 order-lg-1">
-                    <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
-                    <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+                    <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
+                    <p className="font-italic text-muted mb-0 small">{props.description}</p>
                     <div className="d-flex align-items-center justify-content-start mt-1">
-                        <h6 className="font-weight-bold my-2" style={{ marginRight: 30 }}>$120.00</h6>
+                        <h6 className="font-weight-bold my-2" style={{ marginRight: 30 }}>${props.price}</h6>
                         <button type="button" className="btn btn-primary">-</button>
                         <span style={style}>{productCont}</span>
                         <button type="button" className="btn btn-primary">+</button>
-                        <span className={badgeClass + (isAvailable === 'Available' ? 'bg-success' : 'bg-danger')}>{isAvailable}</span>
+                        <span className={badgeClass + (isAvailable === 'Available' ? 'bg-success' : 'bg-danger')}>{props.isAvailable ? "Available" : "UnAvailable"}</span>
                     </div>
-                </div><img src={imageUrl} alt="Generic placeholder image" width="200" height='200' class="ml-lg-5 order-1 order-lg-2" />
+                </div><img src={props.imageUrl} alt="Generic placeholder image" width="200" height='200' class="ml-lg-5 order-1 order-lg-2" />
             </div>
         </li>
     </ul>);
